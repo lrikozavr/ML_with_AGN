a692224089cbe0e1fb0e846a55379fe6e3954c17
lrikozavr <65204033+lrikozavr@users.noreply.github.com>
2020-11-18 08:43:13 +0600

    Add files via upload

---
+++ cut.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IFS=$','
+j=0
+for i in $1
+do
+	echo "$i"
+	cut -c $i $2 | sed 's! !!g' > filename.txt
+	if [ $j -eq 0 ]
+	then 
+		cat filename.txt > $3
+	else
+		cat $3 > filetemp.txt
+		paste -d "\t" filetemp.txt filename.txt > $3	
+	fi
+	j=$((j+1))
+done
+#cat $3 > filetemp.txt
+#../Name.py filetemp.txt $3
+rm filename.txt
+rm filetemp.txt
