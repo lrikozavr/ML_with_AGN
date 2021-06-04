4350eb5e2e264e4f1eac81e274c5af062bce13be
lrikozavr <sorokakiril@gmail.com>
2021-06-04 17:48:46 +0300

    upd: description

--- ML/aaaaaaaa.py
+++ ML/aaaaaaaa.py
@@ -9,6 +9,9 @@ Created on Mon Feb 19 17:01:55 2018
 from keras.layers import Input, Dense
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
+
+from sklearn.model_selection import train_test_split
+
 import numpy as np
 import pandas as pd
 
@@ -59,6 +62,7 @@ def Diff(data):
 				colours[:,index] = data[:,j] - data[:,i]
 				index += 1
 	Result = np.append(data,colours, axis=1)
+	print("Different all Data")
 	print(Result.shape)
 	return Result
 #Выравнивание
@@ -74,6 +78,7 @@ def Rou(data):
 		stds[i] = np.std(data[:,i])
 		Result[:,i] = (data[:,i] - means[i])/stds[i]
 	#print(Result)
+	print("Normalisation Data")
 	return Result
 	
 def DataP(data):
@@ -81,8 +86,8 @@ def DataP(data):
 	data = np.array(data)
 	return Rou(Diff(data))
 
-def NN(train,label,test_size,validation_split,batch_size,num_ep):
-	from sklearn.model_selection import train_test_split
+
+def NN(train,label,test_size,validation_split,batch_size,num_ep,optimizer,loss,output_path_predict,output_path_mod,output_path_weight):
 	X_train, X_test, y_train, y_test = train_test_split(train, label, 
 														test_size = test_size, random_state = 56) #0.4
 	#print(X_train, X_test, y_train, y_test)
@@ -91,7 +96,7 @@ def NN(train,label,test_size,validation_split,batch_size,num_ep):
 	features = train.shape[1]
 	model = DeepDenseNN(features)
 
-	model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+	model.compile(optimizer=optimizer, loss=loss, metrics=['acc'])
 
 	model.fit(X_train, y_train,
 			epochs=num_ep,
@@ -103,7 +108,7 @@ def NN(train,label,test_size,validation_split,batch_size,num_ep):
 	
 	Class = np.array(Class)
 	
-	g=open("/media/kiril/j_08/AGN/excerpt/catalogue/LQAC3_WISE_w1_w4/class.txt",'w')
+	g=open(output_path_predict,'w')
 	Class.tofile(g,"\n")
 	g.close()
 	
@@ -129,88 +134,27 @@ def NN(train,label,test_size,validation_split,batch_size,num_ep):
 				Fs += 1
         
 	print("Accuracy:",              count/y_test.shape[0])
-	print("QSO precision:",     Tq/(Tq+Fq))
-	print("STAR precision:",    Ts/(Ts+Fs))
-	print("QSO completness:",       Tq/(Tq+Fs))
-	print("STAR completness:",     Ts/(Ts+Fq))
-	print("QSO_F:",     2*(Tq/(Tq+Fq)*Tq/(Tq+Fs))/(Tq/(Tq+Fq)+Tq/(Tq+Fs)) )
-	print("STAR_F:",     2*(Ts/(Ts+Fs)*Ts/(Ts+Fq))/(Ts/(Ts+Fq)+Ts/(Ts+Fs)) )
+	print("AGN precision:",     Tq/(Tq+Fq))
+	print("nonAGN precision:",    Ts/(Ts+Fs))
+	print("AGN completness:",       Tq/(Tq+Fs))
+	print("nonAGN completness:",     Ts/(Ts+Fq))
+	print("AGN_F:",     2*(Tq/(Tq+Fq)*Tq/(Tq+Fs))/(Tq/(Tq+Fq)+Tq/(Tq+Fs)) )
+	print("non_AGN_F:",     2*(Ts/(Ts+Fs)*Ts/(Ts+Fq))/(Ts/(Ts+Fq)+Ts/(Ts+Fs)) )
 	
-	SaveModel(model,"/home/kiril/lrikozavr/models/mod1","/home/kiril/lrikozavr/models/weight1")
+	SaveModel(model,output_path_mod,output_path_weight)
 	#return model
 	
-'''
-train_file = '/media/kiril/j_08/AGN/excerpt/catalogue/ex_zempty_name_konf_ALL_nd_phot_1.csv'
 
-data = pd.read_csv(train_file, header=None, sep=',',dtype=np.float)
-data.fillna(0)
-
-#col_l = data.shape[1]-1
-labels=[1 for i in range(data.shape[0])]
-labels = np.array(labels)
-#data = data.drop([col_l], axis=1)
-
-#print(labels)
-print(data.describe())
-stars = data.shape[0]
-mags = data.shape[1]
-print('Stars:', stars)
-print('mags:', mags)
-
-data = np.array(data)
-
-
-mags = data.shape[1]
-num_colours = sum(i for i in range(mags))
-colours = np.zeros((stars,num_colours))
-#print("num_colors",num_colours)
-
-index = 0
-#разница между всеми
-for j in range(mags):
-    for i in range(j, mags):
-        if(i!=j):
-            colours[:,index] = data[:,j] - data[:,i]
-            index += 1
-#print("colours",colours)
-train = np.append(data,colours, axis=1)
-
-print(train.shape)
-
-features = num_colours+mags
-
-means = np.zeros(features)
-stds = np.zeros(features)
-for i in range(data.shape[1]):
-    means[i] = np.mean(train[:,i])
-    stds[i] = np.std(train[:,i])
-    train[:,i] = (train[:,i] - means[i])/stds[i]
-    #print(means[i],stds[i],train[:,i])
-
-#print(train)
-
-#from sklearn.cross_validation import train_test_split
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(train, labels, 
-                                                    test_size = 0.25, random_state = 56)
-
-#print(X_train, X_test, y_train, y_test)
 batch_size = 1024
-num_ep = 15
-
-model = DeepDenseNN(features)
-
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-
-model.fit(X_train, y_train,
-        epochs=num_ep,
-        batch_size=batch_size,
-        validation_split=0.25)
-
-model.summary()
-SaveModel(model,"/home/kiril/lrikozavr/models/mod1","/home/kiril/lrikozavr/models/weight1")
-'''
+optimizer = 'adam'
+loss = 'binary_crossentropy'
+output_path_predict_0 = "/media/kiril/j_08/AGN/excerpt/catalogue/LQAC3_WISE_w1_w4/class.txt"
+output_path_predict_1 = "/media/kiril/j_08/AGN/excerpt/catalogue/LQAC3_WISE_w1_w4/class1.txt"
+output_path_mod = "/home/kiril/lrikozavr/models/mod1"
+output_path_weight = "/home/kiril/lrikozavr/models/weight1"
+input_path_data_test = "/media/kiril/j_08/AGN/excerpt/catalogue/AGN_sdss_n_inf_nonzero.csv"
+input_path_data_train = "/media/kiril/j_08/AGN/excerpt/catalogue/ex_zempty_name_konf_ALL_nd_phot_1.csv"
+input_path_data_trash = "/media/kiril/j_08/AGN/excerpt/catalogue/star_n_inf_nonzero.csv"
 #######################################################################################################################################################
 ##### ###       ## ### ##   # ### ##   #  ####
   #   #  #     # #  #  # #  #  #  # #  # #    #
@@ -218,38 +162,33 @@ SaveModel(model,"/home/kiril/lrikozavr/models/mod1","/home/kiril/lrikozavr/model
   #   #  #   #   #  #  #   ##  #  #   ## #    #
   #   #   # #    # ### #    # ### #    #  ####
 #######################################################################################################################################################
-'''
-train_file = '/media/kiril/j_08/AGN/excerpt/catalogue/ex_zempty_name_konf_ALL_nd_phot_1.csv'
-data_test = pd.read_csv(train_file, header=None, sep=',',dtype=np.float)
 
-#train=DataP(data_test)
-
+data_test = pd.read_csv(input_path_data_train, header=None, sep=',',dtype=np.float)
 label = [1 for i in range(data_test.shape[0])]
 
 
 #dada_test = pd.read_csv("/media/kiril/j_08/AGN/excerpt/catalogue/star_n_inf_nonzero.csv", header=None, sep=',',dtype=np.float)
-dada_test = pd.read_csv("/media/kiril/j_08/AGN/excerpt/catalogue/star_n_inf_nonzero.csv", header=None, sep=',',dtype=np.float)
+dada_test = pd.read_csv(input_path_data_trash, header=None, sep=',', dtype=np.float)
 babel = [0 for i in range(dada_test.shape[0])]
+
 label = np.array(label)
 babel = np.array(babel)
 
 data_test = data_test.append(dada_test, ignore_index=True)
-#data_test.loc[len(data_test.index)] = dada_test
-
 label = np.append(label,babel,axis=0)
-print(data_test.shape[0],data_test.shape[1])
 
-print(np.size(babel))
-print(np.size(label))
+print("Data test shape:	",data_test.shape())
+print("Data dif size:	",np.size(babel),np.size(label))
+print("%",np.size(babel)/np.size(label))
 
 train=DataP(data_test)
-print(train.shape[0],train.shape[1])
+print("Data train shape:	",train.shape())
 
 num_ep = 15
 batch_size = 1024
-NN(train,label,0.25,0.25,batch_size,num_ep)
 
-'''
+NN(train,label,0.25,0.25,batch_size,num_ep,optimizer,loss,output_path_predict,output_path_mod,output_path_weight)
+
 #######################################################################################################################################################
 ##### ###       ## ### ##   # ### ##   #  ####
   #   #  #     # #  #  # #  #  #  # #  # #    #
@@ -260,7 +199,7 @@ NN(train,label,0.25,0.25,batch_size,num_ep)
 ##########
 #data_test = pd.read_csv("/media/kiril/j_08/AGN/excerpt/catalogue/LQAC3_WISE_w1_w4/LQAC3_WISE.csv", header=0, sep=',',dtype=np.float)
 #data_test = data_test.drop(['RA','DEC'], axis=1)
-batch_size = 1024
+
 #######################################################################################################################################################
 ##### ####  ### #####
   #   #    #      #
@@ -268,16 +207,16 @@ batch_size = 1024
   #   #       #   #
   #   #### ###    #
 #######################################################################################################################################################
-data_test = pd.read_csv("/media/kiril/j_08/AGN/excerpt/catalogue/AGN_sdss_n_inf_nonzero.csv", header=None, sep=',',dtype=np.float)
+data_test = pd.read_csv(input_path_data_test, header=None, sep=',', dtype=np.float)
 
 train=DataP(data_test)
-model1 = LoadModel("/home/kiril/lrikozavr/models/mod1","/home/kiril/lrikozavr/models/weight1",'adam','binary_crossentropy')
+model1 = LoadModel(output_path_mod,output_path_weight,optimizer,loss)
 Class = model1.predict(train, batch_size)
 
 
 Class = np.array(Class)
 
-g=open("/media/kiril/j_08/AGN/excerpt/catalogue/LQAC3_WISE_w1_w4/class1.txt",'w')
+g=open(output_path_predict_1,'w')
 Class.tofile(g,"\n")
 g.close()
 
@@ -297,36 +236,3 @@ print("D:	",np.size(Class)/j,"%")
   #   #       #   #
   #   #### ###    #
 #######################################################################################################################################################
-##########
-'''
-label=[1 for i in range(data_test.shape[0])]
-label = np.array(label)
-count = 0
-Tq, Fq, Ts, Fs = 0,0,0,0
-for i in range(label.shape[0]):
-	#print(Tq,Fq,Ts,Fs)
-	if(Class[i]<0.5):
-		Class[i] = 0
-	if(Class[i]>=0.5):
-		Class[i] = 1
-	if(Class[i]==label[i]):
-		count+=1
-	if(Class[i]==1):
-		if(Class[i]==label[i]):
-			Tq += 1
-		else:
-			Fq += 1
-	if(Class[i]==0):
-		if(Class[i]==label[i]):
-			Ts += 1
-		else:
-			Fs += 1
-            
-print("Accuracy:",              count/label.shape[0])
-print("QSO precision:",     Tq/(Tq+Fq))
-print("STAR precision:",    Ts/(Ts+Fs))
-print("QSO completness:",       Tq/(Tq+Fs))
-print("STAR completness:",     Ts/(Ts+Fq))
-print("QSO_F:",     2*(Tq/(Tq+Fq)*Tq/(Tq+Fs))/(Tq/(Tq+Fq)+Tq/(Tq+Fs)) )
-print("STAR_F:",     2*(Ts/(Ts+Fs)*Ts/(Ts+Fq))/(Ts/(Ts+Fq)+Ts/(Ts+Fs)) )
-'''
